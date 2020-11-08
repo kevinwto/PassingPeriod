0468c78b5907a6f868e540103c8c64b5dfb2ea4c
Kevin To <kevinwto01@gmail.com>
2020-11-07 19:50:48 -0800

    final build

--- app/src/main/java/com/example/passingperiod/MainActivity2.java
+++ app/src/main/java/com/example/passingperiod/MainActivity2.java
@@ -5,12 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import java.util.Random;
+
 
 public class MainActivity2 extends AppCompatActivity {
 
+    String[] names = {"Kevin To", "Justin Ho", "Sebastian Garcia", "Fuga Obidi", "Tony Hawk", "Bob Joe", "Alyssa Kordvani",
+            "Mushu Kordvani", "Joe Jonas", "Sam Smith", "Dominic Fike", "Jack Black", "Morgan Freeman", "Jimmy Neutron", "Timmy Turner", "Danny Phantom", "Joe Biden"};;
+
+
     ListView listView;
-    String[] names = {"Kevin To", "Sebastian Garcia"};
-    @Override
+      public static String getRandom() {
+          String[] names = {"Kevin To", "Justin Ho", "Sebastian Garcia", "Fuga Obidi", "Tony Hawk", "Bob Joe", "Alyssa Kordvani",
+                  "Mushu Kordvani", "Joe Jonas", "Sam Smith", "Dominic Fike", "Jack Black", "Morgan Freeman", "Jimmy Neutron", "Timmy Turner", "Danny Phantom", "Joe Biden"};;
+
+          int rnd = new Random().nextInt(names.length);
+        return names[rnd];
+    }
+            @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
