3dad0490f39d4cef1a3f9f87c0eece48d0ba4870
Fuga <oobid001@ucr.edu>
2020-11-07 16:29:40 -0800

    new page for bump

--- app/src/main/java/com/example/passingperiod/FirstFragment.java
+++ app/src/main/java/com/example/passingperiod/FirstFragment.java
@@ -30,5 +30,16 @@ public class FirstFragment extends Fragment {
                         .navigate(R.id.action_FirstFragment_to_SecondFragment);
             }
         });
+
+        view.findViewById(R.id.im_free).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_freeFragment);
+            }
+        });
     }
+
+
+
 }
